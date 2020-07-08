31487ebb43a47c34656c6280480c8950bae1ae79
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-07-08 18:19:26 -0300

    forms

--- resources/views/emprestimo/form.blade.php
+++ resources/views/emprestimo/form.blade.php
@@ -5,16 +5,7 @@
 @endsection('javascript_head')
 
 
-<div class="form-group}}">
-    <label for="data_retirada" class="control-label"><b>Data que foi ou vai ser retirado o Equipamento (pode ser aproximada):</b></label>
-    <input autocomplete="off" class="form-control datepicker" name="data_retirada" type="text" id="data_retirada" value="{{ old('data_retirada',$emprestimo->data_retirada) }}" >
-</div>
-
-<div class="form-group">
-    <label for="patrimonio" class="control-label"><b> Números de Patrimônio dos Equipamentos (se souber): </b></label>
-    <input class="form-control patrimonio" name="patrimonio" type="text" id="patrimonio" value="{{ old('patrimonio',$emprestimo->patrimonio)}}" >
-    <small>Exemplo: 008.034111, 008.037662</small>
-</div>
+@include('emprestimo.partials.form')
 
 <div class="form-group">
     <label for="motivo" class="control-label"><b> Motivo: </b></label>
--- resources/views/emprestimo/form_analise.blade.php
+++ resources/views/emprestimo/form_analise.blade.php
@@ -9,16 +9,8 @@
 {{$emprestimo->motivo}}
 </div>
 <br>
-<div class="form-group}}">
-    <label for="data_retirada" class="control-label"><b>Data que foi ou vai ser retirado o Equipamento:</b></label>
-    <input autocomplete="off" class="form-control datepicker" name="data_retirada" type="text" id="data_retirada" value="{{ old('data_retirada',$emprestimo->data_retirada) }}" >
-</div>
 
-<div class="form-group">
-    <label for="patrimonio" class="control-label"><b> Número de Patrimônio do Equipamento: </b></label>
-    <input class="form-control patrimonio" name="patrimonio" type="text" id="patrimonio" value="{{ old('patrimonio',$emprestimo->patrimonio)}}" >
-    <small>Exemplo: 008.034111</small>
-</div>
+@include('emprestimo.partials.form')
 
 @can('docente')
 <div class="form-group form-check">
